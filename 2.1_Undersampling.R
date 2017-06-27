47fe949a65d30427de155b97c9240d159f6e5fd0
richardparrow <riccardo.parviero@gmail.com>
2017-06-28 01:22:58 +0200

    Undersampling!

---
+++ 2.1_Undersampling.R
@@ -0,0 +1,15 @@
+source("1_import.R")
+
+library(unbalanced)
+
+set.seed(123)
+
+X = train[, -1]
+Y = train$TARG_TOT
+
+under = ubUnder(X, Y, perc = 50, method = "percPos")
+
+newtrain = cbind(under$X, under$Y)
+colnames(newtrain)[74] = "TARG_TOT"
+
+table(newtrain$TARG_TOT)
